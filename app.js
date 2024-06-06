faf39ddc6fb00b05c15d06be8bb363f03624675e
abdulrahmanabualmagd <abdulrahmanabualmagd@gmail.com>
2024-06-06 08:42:19 +0300

    Update

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-const express = require("express");
+// const express = require("express");
 
-const { notFoundHandler } = require("./middlewares/notFoundHandler");
-const { errorHandler } = require("./middlewares/errorHandler");
-const app = express();
+// const { notFoundHandler } = require("./middlewares/notFoundHandler");
+// const { errorHandler } = require("./middlewares/errorHandler");
+// const app = express();
 
-// Parsers
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.text());
-app.use(express.raw());
+// // Parsers
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.text());
+// app.use(express.raw());
 
-// Routes
-app.use("/auth", require("./routers/authRouters"));
-app.use("/home", require("./routers/homeRouters"));
+// // Routes
+// app.use("/auth", require("./routers/authRouters"));
+// app.use("/home", require("./routers/homeRouters"));
 
+// // Handlers
+// app.all("*", notFoundHandler);
+// app.use(errorHandler);
 
-// Handlers
-app.all("*", notFoundHandler);
-app.use(errorHandler);
+// app.listen(process.env.PORT, () => {
+//     console.log(`http://localhost:${process.env.PORT}`);
+// });
 
-app.listen(process.env.PORT, () => {
-    console.log(`http://localhost:${process.env.PORT}`);
-});
+const { dbIdentity } = require("./config/database");
+
+(async () => {
+    const db = await dbIdentity;
+
+    for (let item in db) {
+        console.log(item);
+    }
+
+})();
