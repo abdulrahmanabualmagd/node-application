7829d44c88bd74aefe56793af696f668dfe2829f
abdulrahmanabualmagd <abdulrahmanabualmagd@gmail.com>
2024-06-06 08:40:11 +0300

    Add Main index that choose which index(mongoose/sequelize) to import

---
+++ config/database.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+// Get the env variable to know which datbase you want to use
+const identityPath = `../models/${process.env.IDENTITY_DATABASE}/identity/index`;
+const applicationPath = `../models/${process.env.APPLICATION_DATABASE}/application/index`;
+// Choose the Identity Database (sql, mongodb)
+const dbIdentity = require(identityPath)();
+// Choose the Application Database (sql, mongodb)
+const dbApplication = require(applicationPath)();
+
+module.exports = {
+    dbIdentity,
+    dbApplication,
+};
